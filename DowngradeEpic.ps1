dbaa748cc3aa46504f34046365a6171a6414f14a
twix <91138333+whichtwix@users.noreply.github.com>
2024-03-06 17:36:49 +0600

    update script for new version

--- DowngradeEpic.ps1
+++ DowngradeEpic.ps1
@@ -6,7 +6,7 @@
 Write-Host -BackgroundColor red "Downloading Legendary, please wait..."
 
 # Download Legendary
-curl.exe -LO https://github.com/derrod/legendary/releases/latest/download/legendary.exe
+curl.exe -LO https://github.com/whichtwix/legendary/releases/latest/download/legendary.exe
 
 # This gives Legendary access to the Epic account, this is needed to download the game
 .\legendary auth --import
@@ -16,15 +16,15 @@ Write-Host  -BackgroundColor red "downloading latest among us version first"
 Write-Host  -BackgroundColor red "Please note the 'Install path:' line's folder shown, you will need it later"
 .\legendary install 963137e4c29d4c79a81323b8fab03a40
 
-Invoke-WebRequest -Uri https://github.com/NuclearPowered/Data/raw/de96ca8940c0a16944eba4fbd9ff1c5f3d701f9a/epic/manifests/963137e4c29d4c79a81323b8fab03a40_Windows_3365.manifest -UseBasicParsing -OutFile auman.manifest
+Invoke-WebRequest -Uri https://github.com/whichtwix/Data/raw/master/epic/manifests/963137e4c29d4c79a81323b8fab03a40_2023.11.28.manifest -UseBasicParsing -OutFile auman.manifest
 
 
 Write-Host  -BackgroundColor red "downloading the downgraded version you need"
 Write-Host  -BackgroundColor red "Please note the 'Install path:' line's folder shown, you will need it later"
-.\legendary install 963137e4c29d4c79a81323b8fab03a40 --manifest auman.manifest
+.\legendary install 963137e4c29d4c79a81323b8fab03a40 --manifest auman.manifest --y
 
 Write-Host  -BackgroundColor red "making a quick start file in this folder, click this to start the game"
-'.\legendary launch 963137e4c29d4c79a81323b8fab03a40 --skip-version-check' | Out-File -Encoding ascii -FilePath "2023.7.12.cmd"
+'.\legendary launch 963137e4c29d4c79a81323b8fab03a40 --skip-version-check' | Out-File -Encoding ascii -FilePath "2023.11.28.cmd"
 
 Write-Host "done"
 Write-Host  -BackgroundColor red "Now you must reinstall the mod to the Install path you noted"
