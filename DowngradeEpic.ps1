b867725f9aabbdaf4f74b1a1aa31a6e22e00b846
twix <91138333+whichtwix@users.noreply.github.com>
2024-06-16 13:43:42 +0600

    ready for next update

--- DowngradeEpic.ps1
+++ DowngradeEpic.ps1
@@ -1,4 +1,4 @@
-# This scripts downgrades the Epic Games version to 2023.7.12 using the 3rd party client Legendary
+# This scripts downgrades the Epic Games version to the previous live one using the 3rd party client Legendary
 # 1. Download Legendary
 # 2. Install with the provided manifest
 # 3. Create a small launch script for the game
@@ -12,26 +12,21 @@ curl.exe -LO https://github.com/whichtwix/legendary/releases/latest/download/leg
 .\legendary auth --import
 
 # we have to do this first so the base url can populate as even putting it as a argument later is not enough
-Write-Host  -BackgroundColor red "downloading latest among us version first"
-Write-Host  -BackgroundColor red "Please note the 'Install path:' line's folder shown, you will need it later"
-.\legendary install 963137e4c29d4c79a81323b8fab03a40
+.\legendary install 963137e4c29d4c79a81323b8fab03a40 -n
 
-Invoke-WebRequest -Uri https://github.com/whichtwix/Data/raw/master/epic/manifests/963137e4c29d4c79a81323b8fab03a40_2023.11.28.manifest -UseBasicParsing -OutFile auman.manifest
+Invoke-WebRequest -Uri https://github.com/whichtwix/Data/raw/master/epic/manifests/963137e4c29d4c79a81323b8fab03a40_2024.6.4.manifest -UseBasicParsing -OutFile auman.manifest
 
-
-Write-Host  -BackgroundColor red "downloading the downgraded version you need"
-Write-Host  -BackgroundColor red "Please note the 'Install path:' line's folder shown, you will need it later"
-.\legendary install 963137e4c29d4c79a81323b8fab03a40 --manifest auman.manifest --y
-
-Write-Host  -BackgroundColor red "making a quick start file in this folder, click this to start the game"
-'.\legendary launch 963137e4c29d4c79a81323b8fab03a40 --skip-version-check' | Out-File -Encoding ascii -FilePath "2023.11.28.cmd"
+.\legendary install 963137e4c29d4c79a81323b8fab03a40 --manifest auman.manifest -y
 
 Write-Host "done"
-Write-Host  -BackgroundColor red "Now you must reinstall the mod to the Install path you noted"
 Write-Host  -BackgroundColor red "Attempting to access the new Among Us folder through file explorer"
-Write-Host  -BackgroundColor red "if not found, please find it manually"
+Write-Host  -BackgroundColor red "if not found, please find it manually at C:\users\you\games\among us or similar"
 
 Set-Location -Path (Get-Item -Path $env:USERPROFILE)
 explorer.exe Games\AmongUs
+
+Write-Host  -BackgroundColor red "making a quick start file in the among us folder, click this to start the game"
+Write-Host  -BackgroundColor red "it will be named EpicGamesStarter.exe"
+Invoke-WebRequest -Uri https://github.com/whichtwix/EpicGamesStarter/releases/latest/download/EpicGamesStarter.exe -UseBasicParsing -OutFile Games\AmongUs\EpicGamesStarter.exe
 
 Read-Host
